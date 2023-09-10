54b5faf1ca0789f705adb2d60248b10c2c629578
Travis Cline <travis.cline@gmail.com>
2023-09-09 18:44:37 -0700

    ui: regen

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 // import ExampleCompletion from './ExampleCompletion.tsx'
 import CreateProgram from './CreateProgram.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>woot</h1>
--- ui/src/CreateProgram.tsx
+++ ui/src/CreateProgram.tsx
@@ -33,7 +33,6 @@ subscription Subscription($observeProgramId: ID!) {
       }
     }
   }
-
 }`);
 
 
